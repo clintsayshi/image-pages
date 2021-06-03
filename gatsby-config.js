3f43b66633edcf5be407767ac041eaa7231cf8ad
Clinton Manakane <manakanejustin@gmail.com>
2021-06-03 13:35:40 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  siteMetadata: {
+    title: "image-blog",
+  },
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-sass",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
